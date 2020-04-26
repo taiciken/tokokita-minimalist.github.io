f043b85b70a37d5ed83116d51a895727ebbe0e5e
taiciken <59461913+taiciken@users.noreply.github.com>
2020-04-26 18:12:07 +0700

    Update dontEror.js

--- dontEror.js
+++ dontEror.js
@@ -2,5 +2,5 @@ var dontEror = [
 'jasavideomotiongraphickendal.blogspot.com',
 ];
 
-var redirectUrl = 'https://bit.ly/rianseo_pedia';
+var redirectUrl = 'https://bit.ly/webci_';
 function cekLSN(value,arr){ var status = false; for(var i=0; i<arr.length; i++){ var name = arr[i]; if(name == value){ status = true; break; } } return status; } var hst = window.location.hostname; if(cekLSN(hst, dontEror) == false) { window.location.href = redirectURL; }
