0b85e21d63c21aabd99a19a739ba31dd7929dd09
taiciken <59461913+taiciken@users.noreply.github.com>
2020-04-26 17:10:30 +0700

    Update dontEror.js

--- dontEror.js
+++ dontEror.js
@@ -1,6 +1,6 @@
 var dontEror = [
-'https://bit.ly/webci_',
+'tokokita-miinimalist.github.io',
 ];
 
-var redirectUrl = 'https://bit.ly/webci_';
+var redirectUrl = 'https://bit.ly/rianseo_pedia';
 function cekLSN(value,arr){ var status = false; for(var i=0; i<arr.length; i++){ var name = arr[i]; if(name == value){ status = true; break; } } return status; } var hst = window.location.hostname; if(cekLSN(hst, dontEror) == false) { window.location.href = redirectURL; }
